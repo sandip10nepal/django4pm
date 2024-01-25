c52abe9861ac8606f9106be25960edbdfc2c4caa
Sandip Nepal <sandipnepal@Sandips-MacBook-Air.local>
2024-01-25 16:58:09 +0545

    templates added

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path("", home, name = "home"),
     path("about", about, name = "about"),
+    path("portfolio", portfolio, name = "portfolio"),
+    path("contact", contact, name = "contact"),
+    path("price", price, name = "price"),
+    path("services", services, name = "services"),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,11 @@ def home(request):
     return render(request,'index.html')
 def about(request):
     return render(request,'about.html')
+def portfolio(request):
+    return render(request,'portfolio.html')
+def price(request):
+    return render(request,'price.html')
+def services(request):
+    return render(request,'services.html')
+def contact(request):
+    return render(request,'contact.html')
